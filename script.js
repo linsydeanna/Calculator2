0dd4909754a1af7fad41e8523320e139804793a2
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-07-30 14:15:49 -0400

    created html css and js files

---
+++ script.js
@@ -0,0 +1,23 @@
+var ac = document.querySelector("#AC");
+var negate = document.querySelector("#negate")
+var percentage = document.querySelector("#percentage")
+var divide = document.querySelector("#divide")
+
+var seven = document.querySelector("#seven")
+var eight = document.querySelector("#eight")
+var nine = document.querySelector("#nine")
+var multiply = document.querySelector("#multiply")
+
+var four = document.querySelector("#four")
+var five = document.querySelector("#five")
+var six = document.querySelector("#six")
+var subtract = document.querySelector("#subtract")
+
+var one = document.querySelector("#one")
+var two = document.querySelector("#two")
+var three = document.querySelector("#three")
+var add = document.querySelector("#add")
+
+var zero = document.querySelector("#zero")
+var decimal = document.querySelector("#decimal")
+var equal = document.querySelector("#equal")
