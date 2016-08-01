c8b0b0651afa8244670a5242dbd382f21f169ed6
Linsy Joyner <Linsy.Joyner@gmail.com>
2016-08-01 13:42:20 -0400

    refractor

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 
-var sevenbutton = document.getElementById("sevenbutton");
-var eightbutton = document.getElementById("eightbutton");
-var ninebutton = document.getElementById("ninebutton");
-var fourbutton = document.getElementById("fourbutton");
-var fivebutton = document.getElementById("fivebutton");
-var sixbutton = document.getElementById("sixbutton");
-var onebutton = document.getElementById("onebutton");
-var twobutton = document.getElementById("twobutton");
-var threebutton = document.getElementById("threebutton");
-var zerobutton = document.getElementById("zerobutton");
+
 var equalbutton = document.getElementById("equalbutton");
 var addbutton = document.getElementById("addbutton");
 var subtractbutton = document.getElementById("subtractbutton");
@@ -18,54 +9,15 @@ var clearbutton = document.getElementById("clearbutton");
 
 var allnumbers = [];
 
-sevenbutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "7";
-  allnumbers.push(7);
-});
+var numbers = document.querySelectorAll(".number");
 
-eightbutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "8";
-  allnumbers.push(8);
-});
-
-ninebutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "9";
-  allnumbers.push(9);
-});
-
-fourbutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "4";
-  allnumbers.push(4);
-});
-
-fivebutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "5";
-  allnumbers.push(5);
-});
-
-sixbutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "6";
-  allnumbers.push(6);
-});
-
-onebutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "1";
-  allnumbers.push(1);
-});
-
-twobutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "2";
-  allnumbers.push(2);
-});
-
-threebutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "3";
-  allnumbers.push(3);
-});
-
-zerobutton.addEventListener("click", function(event){
-  document.getElementById("screen").value += "0";
-});
+for (var i = 0;i < numbers.length;i++) {
+  numbers[i].addEventListener("click", function(event){
+    var valueAsInteger = parseInt(event.target.value);
+    allnumbers.push(valueAsInteger);
+    document.getElementById("screen").value += event.target.value;
+  })
+};
 
 addbutton.addEventListener("click", function(event){
   document.getElementById("screen").value += "+";
@@ -99,6 +51,7 @@ equalbutton.addEventListener("click", function(event){
   } else if (allnumbers[1] === "*") {
     var result = a * b;
   }
+  console.log(allnumbers);
   document.getElementById("screen").value = result;
 });
 
